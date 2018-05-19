7e509deabdf2ae42c9aa4b7cb51a47e1992607ff
yangjiajun <yangjiajun999@126.com>
2018-05-19 16:45:21 +0800

    初始化项目

---
+++ framework-auth/src/main/java/com/framework/springcloud/FrameworkAuthApplication.java
@@ -0,0 +1,11 @@
+package com.framework.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkAuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FrameworkAuthApplication.class, args);
+    }
+}
---
+++ framework-auth/src/main/java/com/framework/springcloud/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.framework.springcloud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/")
+public class HelloController {
+
+    @GetMapping(value = "/hello")
+    public String hello() {
+        return "hello world ! Success !";
+    }
+}
