b967782c7671a847d15750b0e9a4441fd4014349
yangjiajun <yangjiajun999@126.com>
2018-06-08 18:06:49 +0800

    修改文件名

--- framework-oauth/src/main/java/com/framework/springcloud/controller/HelloController.java
+++ framework-oauth/src/main/java/com/framework/springcloud/controller/HelloController.java
@@ -1,15 +1,22 @@
 package com.framework.springcloud.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 @RequestMapping(value = "/")
 public class HelloController {
+    @Autowired
+    private StringRedisTemplate redisTemplate;
 
     @GetMapping(value = "/hello")
     public String hello() {
-        return "hello world ! Success !";
+        redisTemplate.opsForValue().set("yangjiajun", "30", 1, TimeUnit.MINUTES);
+        return redisTemplate.opsForValue().get("yangjiajun");
     }
 }
